54183efd5b1e483cf608ce0dd0687f8d6df1e603
Judy <judychue@pursuit.org>
2023-08-06 19:00:51 -0400

    seed list complete

--- db/seed.sql
+++ db/seed.sql
@@ -3,9 +3,12 @@
 INSERT INTO socialmedia (name, rating, launched, ma_users, website, is_favorite) VALUES
 
 ('Facebook', '1', '2004', '2.9B', "https://www.facebook.com/"),
-('Fame', 'David Bowie', 'Young Americans', '4:12', false),
--- ('Waterfalls', 'TLC', 'CrazySexyCool', '3:44', true),
--- ('Rock With You', 'Michael Jackson', 'Off the Wall', '3:23', true),
--- ('Do You Really Want To Hurt Me', 'Culture Club', 'Kissing To Be Clever', '3:00', true),
--- ('Vivir Mi Vida', 'Marc Anthony', 'Marc Anthony 3.0', '5:26', true),
--- ('Kill Bill', 'SZA', 'SOS', '2:34', true)
+('YouTube', '2', '2005', '2.2B', "https://www.youtube.com/"),
+('WhatsApp', '3', '2009', '2B', "https://web.whatsapp.com/"),
+('Instagram', '4', '2010', '2B', "https://www.instagram.com/"),
+('TikTok', '5', '2016', '1B', "https://www.tiktok.com/en/"),
+('Snapchat', '6', '2011', '538M', "https://www.snapchat.com/"),
+('Pinterest', '7', '2005', '444M', "https://www.pinterest.com/pinterest/")
+('Reddit', '8', '2010', '430M', "https://www.reddit.com/")
+('LinkedIn', '9', '2006', '250M', "https://www.linkedin.com/")
+('X', '10', '2003', '217M', "https://twitter.com/")
