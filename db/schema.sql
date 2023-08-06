9a67a2591a821bd3ae611c0576ddad31398dc048
Judy <judychue@pursuit.org>
2023-08-06 17:40:37 -0400

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS socialmedia_dev;
+CREATE DATABASE socialmedia_dev;
+
+\c socialmedia_dev;
+
+-- DROP TABLE IF EXISTS socialmedia;
+-- CREATE TABLE socialmedia (
+--  id SERIAL PRIMARY KEY,
+--  name TEXT NOT NULL,
+--  artist TEXT,
+--  album TEXT,
+--  time TEXT,
+--  is_favorite BOOLEAN
+-- );
+
+DROP TABLE IF EXISTS socialmedia;
+CREATE TABLE socialmedia (
+ id SERIAL PRIMARY KEY,
+ name TEXT NOT NULL,
+ rating TEXT,
+ launched TEXT,
+ ma_users TEXT,
+ website TEXT,
+ is_favorite BOOLEAN
+);
---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+\c socialmedia_dev;
+
+INSERT INTO socialmedia (name, rating, launched, ma_users, website, is_favorite) VALUES
+
+('Facebook', '1', '2004', '2.9B', "https://www.facebook.com/"),
+('Fame', 'David Bowie', 'Young Americans', '4:12', false),
+-- ('Waterfalls', 'TLC', 'CrazySexyCool', '3:44', true),
+-- ('Rock With You', 'Michael Jackson', 'Off the Wall', '3:23', true),
+-- ('Do You Really Want To Hurt Me', 'Culture Club', 'Kissing To Be Clever', '3:00', true),
+-- ('Vivir Mi Vida', 'Marc Anthony', 'Marc Anthony 3.0', '5:26', true),
+-- ('Kill Bill', 'SZA', 'SOS', '2:34', true)
